aca95e85b48100fa83f6713252733d9ec81f1c4e
Veenith31 <147136881+Veenith31@users.noreply.github.com>
2025-09-13 16:02:28 +0530

    Update route.ts

--- src/app/api/tenants/[slug]/upgrade/route.ts
+++ src/app/api/tenants/[slug]/upgrade/route.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth/next";
 import { NextResponse, NextRequest } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import { authOptions } from "../../../../auth/[...nextauth]/route";
+import { authOptions } from "../../../auth/[...nextauth]/route";
 
 const prisma = new PrismaClient();
 
