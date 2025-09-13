6e8b73134779b61dcd39cb25c1564c6c7483df44
Veenith Kumar S <veeniths31@example.com>
2025-09-13 21:47:52 +0530

    fix: Resolve redirect loop in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,12 @@
-export { default } from "next-auth/middleware";
+//export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
+
 
 export const config = {
   matcher: [
